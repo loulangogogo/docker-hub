37a3e70a116eddc2d39cf513b58a27d2c9e95710
loulan <loulangogogo@163.com>
2025-03-27 17:58:36 +0800

    添加版本

---
+++ xxl-job/2.5.0/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 使用 exec 直接执行 Java 进程（避免额外 bash 子进程）
+exec java ${JAVA_OPTS} \
+  -jar /app.jar \
+  ${PARAMS} \
+  --server.port="${SERVER_PORT:-8080}" \
+  --server.servlet.context-path="${SERVER_PATH:-/xxl-job-admin}" \
+  --spring.datasource.url="jdbc:mysql://${DB_HOST:-127.0.0.1}:${DB_PORT:-3306}/${DB_NAME:-xxl-job}?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&serverTimezone=Asia/Shanghai" \
+  --spring.datasource.username="${DB_USER:-root}" \
+  --spring.datasource.password="${DB_PASSWORD:-123456}" \
+  --spring.mail.host="${MAIL_HOST:-smtp.qq.com}" \
+  --spring.mail.port="${MAIL_PORT:-25}" \
+  --spring.mail.username="${MAIL_USER:-xxx}" \
+  --spring.mail.from="${MAIL_FROM:-xxx}" \
+  --spring.mail.password="${MAIL_PASSWORD:-xxx}" \
+  --xxl.job.accessToken="${JOB_ACCESS_TOKEN:-default_token}"
